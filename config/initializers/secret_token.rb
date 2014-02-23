b3d06a87ca6f7228d3cd1ff1d120779034136f67
Fasterbot <fasterbot@hotmail.com>
2014-02-23 16:10:00 +0100

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'a56818d187b4f5ec30f83b63445ea3ba57c0b6a82740933b8c884b7a35452b2c9e51e6327d19df88a01473d88ce1e4a0ab78460a9f02d2e1c67f1076f5b329cc'
